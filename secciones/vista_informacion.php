2f48610800c1d4488f41d913fcc7312f5356954d
AdrianJose210705 <adrianjosefigueroasalazar6@gmail.com>
2026-03-04 21:54:02 -0400

    Se agrego

--- secciones/vista_informacion.php
+++ secciones/vista_informacion.php
@@ -1,62 +1,89 @@
 <?php include('../template/cabecera.php')?>
 <?php include('../secciones/informacion.php')?>
 
-
+<?php foreach($listaNombre2 as $nombre){?>
 <div class="container">
     <div class="row">
         <div class="col-12 color_principal text-center">
-        <p>CV</p>
         <h1>Creadores</h1>
         <p><span>---</span>Estudiantes de Ing. Computacion<span>---</span></p>
+        <div class="container text-center">
+            <div class="row align-items-start">
+                <div class="col">
+                <p>
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#000000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round">
+                    <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                </svg>
+                (+58 0412-9459012)
+                </p>
+                </div>
+                <div class="col">
+                <p>              
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#000000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round">
+                    <path d="M16 20h3a1 1 0 0 0 1 -1v-14a1 1 0 0 0 -1 -1h-3v16z" />
+                    <path d="M5 20h3v-16h-3a1 1 0 0 0 -1 1v14a1 1 0 0 0 1 1z" />
+                    <path d="M16 4l-4 4l-4 -4" />
+                    <path d="M4 6.5l8 7.5l8 -7.5" />
+                </svg>
+                adrianjosefigueroasalazar6@gmail.com
+                </p>
+                </div>
+                <div class="col">
+                <p>
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#000000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round">
+                    <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                    <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                </svg>
+                Venezuela
+                </p>
+                </div>
+            </div>
         </div>
-        <div class="col-4 color_izquierdo">.col-4<br>
-
-
-
         </div>
-        <div class="col-8 colo_derecho">.col-6<br>
-
-
-
-
+        <div class="col-4 color_izquierdo color_izquierdo img">
+            <br>
+            <img  style="border-radius: 20%;" src="../fotoperfil/Foto.jpg" alt="Imagen de perfil" class="rounded mx-auto d-block">
+            <br>
+            <p class="text-center fst-italic text-light fs-5">Estudiante de la universidad de oriente nucleo de anzoategui aspirante a titulo de Ingeniero de Computacion.</p>
+        </div>
+        <div class="col-8 color_derecho">
+            <p class="fs-3 text-light">Datos:</p>
+            <p class="fs-5">Nombre: <?php echo $nombre['nombre_alumno']?></p>
+            <p class="fs-5">Cedula: <?php echo $nombre['cedula']?></p>
+            <p class="fs-5">Universidad: <?php echo $nombre['universidad']?></p>
+            <p class="fs-5">Carrera: <?php echo $nombre['carrera']?></p>
+            <p class="fs-5">Edad: <?php echo $nombre['bio']?></p>
+            <p class="fs-5">Lenguajes de manejo: <?php echo $nombre['habilidades']?></p>
         </div>
     </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <tbody>
-            <?php foreach($listaNombre2 as $nombre){?>
-                <tr>
-                    <td><?php echo $nombre ['nombre_alumno']?></td>
-                    <td>CI</td>
-                    <td>Universidad</td>
-                    <td>Carerra Universitaria</td>
-                    <td><?php echo $nombre ['foto_url']?></td>
-                    <td>Habilidades</td>
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
-    </div>
-    
-
-</div>
+<?php } ?>
 
 <?php include('../template/pie.php')?>
